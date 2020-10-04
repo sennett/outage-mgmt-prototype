7cc44f0cbaa0db0ffdc3bdf0e1ed157d96567313
sennett <qaplwsokedijrfuhtg6>
2020-10-04 22:34:43 +0100

    tidyup

--- rxjs-spikes.test.js
+++ rxjs-spikes.test.js
@@ -1,6 +1,6 @@
 const { TestScheduler } = require('rxjs/testing')
-const { groupBy, flatMap, map, scan, bufferWhen, filter, delay, merge, takeUntil, count, mapTo, mergeAll, tap } = require('rxjs/operators')
-const { interval, of, from } = require('rxjs')
+const { groupBy, flatMap, map, scan, filter, delay, merge, takeUntil, count, mapTo, mergeAll } = require('rxjs/operators')
+const { of } = require('rxjs')
 
 const getTestScheduler = () => (new TestScheduler((received, expected) => {
   // console.log(received)
@@ -31,41 +31,23 @@ describe('rxjs spikes', () => {
       expectObservable(received).toBe(expected)
     })
   })
-  describe('emits events that only happen three times within 5 ms', () => {
-    it.only('bufferWhen', () => {
-      const testScheduler = getTestScheduler()
-      testScheduler.run(({ cold, hot, expectObservable }) => {
-        const source = hot('aaa-- bbb-- cc--- -dd-- -eee- ----- --fff -----|')
-        const expected = '  ----- a---- b---- ----- ----- -e--- ----- --f--|'
+  it('emits events that only happen three times within 5 ms', () => {
+    const testScheduler = getTestScheduler()
+    testScheduler.run(({ cold, hot, expectObservable }) => {
+      const source = hot('aaa-- bbb-- cc--- -dd-- -eee- ----- --fff -----|')
+      const expected = '  ----- a---- b---- ----- ----- -e--- ----- --f--|'
 
-        const received = source.pipe(
-          map(v => of(v).pipe(
-            merge(source),
-            takeUntil(of('anything').pipe(delay(5, testScheduler))),
-            count(sourceValue => sourceValue === v),
-            filter(count => count >= 3),
-            mapTo(v)
-          )),
-          mergeAll()
-        )
-
-        expectObservable(received).toBe(expected)
-      })
-    })
-  })
-
-  it.skip('bufferTime', () => {
-    getTestScheduler().run(({ cold, expectObservable }) => {
-      const source = cold('-a-a-a----a-a-a-')
-      const expected = '   -----(1-2-4-6-9-'
       const received = source.pipe(
-        groupBy(v => v % 2),
-        map(div2Stream => div2Stream
-          .pipe(
-            scan((acc, cur) => acc + parseInt(cur), 0),
-            map(v => v.toString()))),
-        flatMap(v => v)
+        map(v => of(v).pipe(
+          merge(source),
+          takeUntil(of('anything').pipe(delay(5, testScheduler))),
+          count(sourceValue => sourceValue === v),
+          filter(count => count >= 3),
+          mapTo(v)
+        )),
+        mergeAll()
       )
+
       expectObservable(received).toBe(expected)
     })
   })
