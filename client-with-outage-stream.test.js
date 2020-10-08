cf3304ec14f41ba65530fe19ecc7e09116365c41
sennett <qaplwsokedijrfuhtg6>
2020-10-08 15:52:22 +0100

    does not flag if out for 29 continuous seconds

--- client-with-outage-stream.test.js
+++ client-with-outage-stream.test.js
@@ -1,13 +1,13 @@
 const { TestScheduler } = require('rxjs/testing')
 const clientWithOutageStream = require('./client-with-outage-stream')
 
-const testScheduler = new TestScheduler((recevied, expected) => {
+const buildTestScheduler = () => new TestScheduler((recevied, expected) => {
   expect(recevied).toEqual(expected)
 })
 
 describe('client-with-outage-stream', () => {
   it('does not flag if client does not have an outage', () => {
-    testScheduler.run(({ cold, expectObservable }) => {
+    buildTestScheduler().run(({ cold, expectObservable }) => {
       const values = {
         a: {
           firstName: 'Mary no outage',
@@ -21,7 +21,7 @@ describe('client-with-outage-stream', () => {
   })
 
   it('does not flag if just one hasOutage', () => {
-    testScheduler.run(({ cold, expectObservable }) => {
+    buildTestScheduler().run(({ cold, expectObservable }) => {
       const values = {
         a: {
           id: 'client_id',
@@ -35,7 +35,23 @@ describe('client-with-outage-stream', () => {
     })
   })
 
-  it.todo('does not flat if hasOutage for 29 continuous seconds')
+  it('does not flag if hasOutage for 29 continuous seconds', () => {
+    buildTestScheduler().run(({ cold, expectObservable }) => {
+      const values = {
+        a: {
+          id: 'client_id',
+          firstName: 'Tony Outage',
+          hasOutage: true
+        }
+      }
+
+      const clientStream = cold(`- ${'a 1s '.repeat(29)} ----`, values)
+      const expected = `         - ${'- 1s '.repeat(29)} ----`
+      expectObservable(clientWithOutageStream(clientStream)).toBe(expected)
+    })
+  })
+
   it.todo('flags if hasOutage for 30 continuous seconds')
-  it.todo('does not flag if !hasOutage for 5 continuous minutes')
+  it.todo('does not flag if !hasOutage for 30 continuous seconds')
+  it.todo('does not flag if !hasOutage once in 35 seconds')
 })
