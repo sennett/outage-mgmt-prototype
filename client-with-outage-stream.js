9028c35fd1a4e0b65b298dffa5c5232d20d17237
sennett <qaplwsokedijrfuhtg6>
2020-10-15 13:07:44 +0100

    clilent with outage stream refactor

--- client-with-outage-stream.js
+++ client-with-outage-stream.js
@@ -1,31 +1,23 @@
 const { filter, merge, takeUntil, distinctUntilChanged, flatMap, count, delay, mapTo } = require('rxjs/operators')
 const { of } = require('rxjs')
 
+const extractFirstEventOf30ContinuousSeconds = (interestingEvents) => interestingEvents
+  .pipe(
+    flatMap(firstInterestingEvent => of(firstInterestingEvent).pipe(
+      merge(interestingEvents),
+      takeUntil(of('anything').pipe(delay(30000))),
+      count(),
+      filter(count => count >= 30),
+      mapTo(firstInterestingEvent)
+    ))
+  )
+
 module.exports = (clientEvents) => {
   const outageEvents = clientEvents.pipe(filter(client => client.hasOutage))
-  const continuousOutageSignals =
-    outageEvents
-      .pipe(
-        flatMap(firstOutageEvent => of(firstOutageEvent).pipe(
-          merge(outageEvents),
-          takeUntil(of('anything').pipe(delay(30000))),
-          count(),
-          filter(count => count >= 30),
-          mapTo(firstOutageEvent)
-        ))
-      )
+  const continuousOutageSignals = extractFirstEventOf30ContinuousSeconds(outageEvents)
 
   const uppageEvents = clientEvents.pipe(filter(client => !client.hasOutage))
-  const continuousUppageSignals = uppageEvents
-    .pipe(
-      flatMap(firstUppageEvent => of(firstUppageEvent).pipe(
-        merge(uppageEvents),
-        takeUntil(of('anything').pipe(delay(30000))),
-        count(),
-        filter(count => count >= 30),
-        mapTo(firstUppageEvent)
-      ))
-    )
+  const continuousUppageSignals = extractFirstEventOf30ContinuousSeconds(uppageEvents)
 
   return continuousOutageSignals.pipe(
     merge(continuousUppageSignals),
