b080da7e6f6c23387f1c7aa6ecb482341e97f939
sennett <qaplwsokedijrfuhtg6>
2020-10-15 13:01:00 +0100

    tidy up client stream

--- client-stream.js
+++ client-stream.js
@@ -2,20 +2,20 @@ const { from, timer } = require('rxjs')
 const { map, flatMap } = require('rxjs/operators')
 const got = require('got')
 
+const request = () => from(got(`${process.env.CRM_DOMAIN}/crm/api/v1.0/clients`, {
+  headers: {
+    Accept: 'application/json',
+    'x-auth-token': process.env.CRM_API_KEY
+  },
+  searchParams: {
+    isArchived: 0,
+    lead: 0
+  }
+}
+)).pipe(map(response => JSON.parse(response.body)))
+
 module.exports = () => timer(0, 1000)
   .pipe(
-    map(() => from(got(`${process.env.CRM_DOMAIN}/crm/api/v1.0/clients`, {
-      headers: {
-        Accept: 'application/json',
-        'x-auth-token': process.env.CRM_API_KEY
-      },
-      searchParams: {
-        isArchived: 0,
-        lead: 0
-      }
-    }))
-      .pipe(map(response => JSON.parse(response.body)))),
-    flatMap(value => value),
-    map(listOfClients => from(listOfClients)),
-    flatMap(value => value)
+    flatMap(request),
+    flatMap(listOfClients => from(listOfClients))
   )
