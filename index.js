97a5c5a87377653d7bdb1db95c526665477e74f2
sennett <qaplwsokedijrfuhtg6>
2020-10-17 16:10:18 +0100

    keepalive server

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config()
 const app = require('./app')
+const logger = require('./logger')
 
 app()
+
+const fastify = require('fastify')()
+
+fastify.get('/keep-alive', async (request, reply) => {
+  return 'running!'
+})
+
+const startServer = async () => {
+  await fastify.listen(3000)
+  logger.info(`running server on ${fastify.server.address().port}`)
+}
+
+startServer()
