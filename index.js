f6e0a860779025d02ff5da30113a3bea84392921
sennett <qaplwsokedijrfuhtg6>
2020-10-18 11:39:26 +0100

    front-end

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 require('dotenv').config()
+const path = require('path')
+const fastify = require('fastify')()
+const fastifyStatic = require('fastify-static')
 const outageDetection = require('./outage-detection')
 const logger = require('./logger')
 
-outageDetection()
-
-const fastify = require('fastify')()
-
-fastify.get('/keep-alive', async (request, reply) => {
-  return 'running!'
-})
-
 const startServer = async () => {
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, 'frontend')
+  })
+  fastify.get('/keep-alive', async (request, reply) => {
+    return 'running!'
+  })
   await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
   logger.info(`running server on ${fastify.server.address().port}`)
 }
+
+outageDetection()
 
 startServer()
