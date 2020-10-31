07cbfdf0bc55372d79aabba5efc54ce5919e7044
sennett <qaplwsokedijrfuhtg6>
2020-10-31 22:43:11 +0000

    update to DATABASE_URL

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const knex = require('knex')({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
+  connection: process.env.DATABASE_URL,
   migrations: {
     tableName: 'migrations',
     directory: path.join(__dirname, 'migrations')
